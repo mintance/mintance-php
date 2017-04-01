debc3e540a59d7933efa5d7927efe34a9aa067fe
Peter Borodatyy <peterborodatyy@gmail.com>
2017-04-02 02:55:31 +0300

    Event loop dispatcher.

---
+++ src/Core/DispatcherLoop.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Mintance\Core;
+
+use Mintance\Exceptions\Exception;
+
+abstract class DispatcherLoop {
+
+	protected $_subscribers = [];
+
+	public function __call($name, $args) {
+		if(!method_exists($this, '_'.$name)) {
+			throw new Exception('Method is undefined.');
+		}
+
+		$args = $args[0];
+
+		if(!empty($this->_subscribers[$name.':before'])) {
+			foreach ($this->_subscribers[$name.':before'] as $subscriber) {
+				$subscriber($args);
+			}
+		}
+
+		$response = $this->{'_'.$name}($args);
+
+		if(!empty($this->_subscribers[$name.':after'])) {
+			foreach ($this->_subscribers[$name.':after'] as $subscriber) {
+				$subscriber($response);
+			}
+		}
+	}
+
+	public function register($event, callable $callback) {
+		if(empty($this->_subscribers[$event])) {
+			$this->_subscribers[$event] = [];
+		}
+
+		$this->_subscribers[$event][] = $callback;
+	}
+
+	public function clear() {
+		$this->_subscribers = [];
+	}
+}
