5c9e8f286d487087f037b3224c81e17b5b54f6eb
Peter Borodatyy <peterborodatyy@gmail.com>
2017-09-05 16:09:43 +0300

    Autoloader fix.

--- autoloader.php
+++ autoloader.php
@@ -1,7 +1,33 @@
 <?php
+
 /**
- * Created by PhpStorm.
- * User: peterborodatyy
- * Date: 9/5/17
- * Time: 15:58
+ * Mintance library autoloader.
  */
+spl_autoload_register(function ($class) {
+
+	// project-specific namespace prefix
+	$prefix = 'Mintance\\';
+
+	// base directory for the namespace prefix
+	$base_dir = __DIR__ . '/src/';
+
+	// does the class use the namespace prefix?
+	$len = strlen($prefix);
+	if (strncmp($prefix, $class, $len) !== 0) {
+		// no, move to the next registered autoloader
+		return;
+	}
+
+	// get the relative class name
+	$relative_class = substr($class, $len);
+
+	// replace the namespace prefix with the base directory, replace namespace
+	// separators with directory separators in the relative class name, append
+	// with .php
+	$file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+	// if the file exists, require it
+	if (file_exists($file)) {
+		require $file;
+	}
+});
